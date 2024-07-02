9cf1cb4713f38dc89f078765c72a8857ac2d420a
Will Zhang <shaozhou.zhang@external.iherb.com>
2024-07-02 14:43:43 +0800

    first commit

---
+++ DragonQuest/DragonQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonQuestApp.swift
+//  DragonQuest
+//
+//  Created by Will Zhang on 2024/7/2.
+//
+
+import SwiftUI
+
+@main
+struct DragonQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
